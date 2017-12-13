e6c054eaac5ed618bf23cd9c87c3b79c92f43e21
hoangtuyb96 <hoangtuyb96@gmail.com>
2017-12-13 11:11:49 +0700

    init_database

---
+++ db/migrate/20171213034755_create_notifications.rb
@@ -0,0 +1,10 @@
+class CreateNotifications < ActiveRecord::Migration[5.1]
+  def change
+    create_table :notifications do |t|
+      t.integer :user_id
+      t.integer :notificationable_id
+      t.string :notificationable_type
+      t.string :content
+    end
+  end
+end
---
+++ db/migrate/20171213035030_create_group_members.rb
@@ -0,0 +1,9 @@
+class CreateGroupMembers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :group_members do |t|
+      t.integer :group_id
+      t.integer :user_id
+      t.integer :permission
+    end
+  end
+end
