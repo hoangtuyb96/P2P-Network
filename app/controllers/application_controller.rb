fd5f129536b029bd3d7dff1930f9a7f4f4bf1ae0
hoangtuyb96 <hoangtuyb96@gmail.com>
2017-12-23 00:05:48 +0700

    notification_when_followed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :index_notifications
 
   def check_logged_in
     unless user_signed_in?
       redirect_to login_path
       flash[:danger] = t("devise.failure.unauthenticated")
+    end
+  end
+
+  def index_notifications
+    if user_signed_in?
+      @notifications = current_user.notifications.limit(15)
     end
   end
 end
